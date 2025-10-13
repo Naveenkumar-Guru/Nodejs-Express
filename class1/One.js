020a32854115acc94a2c02e498e03df16f16eb53
Naveenkumar506 <naveennani0603@gmail.com>
2025-10-13 19:13:52 +0530

    node install

---
+++ class1/One.js
@@ -0,0 +1,7 @@
+const os = require("os");
+
+console.log("Platform:", os.platform());
+console.log(os.arch());
+console.log(os.homedir());
+console.log(os.freemem());
+console.log(os.release());
---
+++ class1/Two.js
@@ -0,0 +1,7 @@
+import os from "os";
+
+console.log("Platform:", os.platform());
+console.log("Architecture:", os.arch());
+console.log("Home Directory:", os.homedir());
+console.log("Free Memory (bytes):", os.freemem());
+console.log("OS Release:", os.release()); 
