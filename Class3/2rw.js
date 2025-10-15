4c2261ff52f103f147414c70637f9b018e62f040
Naveenkumar506 <naveennani0603@gmail.com>
2025-10-15 23:37:10 +0530

    sync files

--- Class3/2rw.js
+++ Class3/2rw.js
@@ -1,12 +1,11 @@
-
 import fs from "fs";
 
-fs.readFile("1Employee.json", "utf-8",(error,data)=>{
-    if(error) throw error
-    let employee=JSON.parse(data)
-    let male_employees=employees.filter((emp)=>{
-        return employee.gender==="Male"
-    })
-    console.log(male_employees.length)
+fs.readFile("1Employee.json", "utf-8", (error, data) => {
+  if (error) throw error;
 
-};
+  let employees = JSON.parse(data);
+
+  let male_employees = employees.filter((emp) => emp.gender === "Male");
+
+  console.log(male_employees.length);
+});
