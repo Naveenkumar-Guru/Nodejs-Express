9410d23d564b8021d5f00e7922e08440c19966dc
Naveenkumar506 <naveennani0603@gmail.com>
2025-10-30 14:18:18 +0530

    saveEmployee

--- Class9Express-4/routes/empRouter.js
+++ Class9Express-4/routes/empRouter.js
@@ -3,14 +3,43 @@ import fs from "fs";
 import path from "path";
 let router = express.Router();
 
-router.post("/emp/create", (req, resp) => {
-  return resp.json({ msg: "hlooo" });
-});
+// router.get("/create", (req, resp) => {
+//   return resp.json({ msg: "hlooo" });
+// });
 
+/*
+create
+-------
+usage: Create new employee
+Rest API URL:http://127.0.0.1:8080/emp/create
+Method Type:POST
+Required Fields: eid,ename,esal,loc
+Access Type:Public
+*/
+
+router.post("/create", async (req, resp) => {
+  console.log("Inside POST Method");
+  let emp = req.body;
+  console.log(emp);
+  let employees = await getEmployees();
+  console.log(employees.length);
+  let employee = employees.find((employee) => {
+    return employee.eid === emp.eid;
+  });
+  console.log(employee);
+  if (employee) {
+    return resp.status(404).json({ msg: "Buddy! Employee Already Exists" });
+  }
+
+  employees.push(emp);
+  await saveEmployee(employees);
+  return resp.status(200).json({ msg: "new employee is create successfully" });
+});
 router.get("/read", async (req, resp) => {
-  console.log("Inside Read Request...");
+  console.log("Inside post Method...");
   let employees = await getEmployees();
   return resp.status(200).json(employees);
+  // return resp.status(200).send(JSON.stringify(employees, null, 2));
 });
 
 let getEmployees = () => {
@@ -20,4 +49,10 @@ let getEmployees = () => {
   return JSON.parse(emp_Data);
 };
 
+let saveEmployee = (employees) => {
+  let emp_file = path.join(process.cwd(), "data", "employees.json");
+  console.log(emp_file);
+  let emp_Data=fs.writeFileSync(emp_file, JSON.stringify(employees, null, 2), "utf-8");
+  return JSON.parse(emp_Data);
+};
 export default router;
--- Class9Express-4/server.js
+++ Class9Express-4/server.js
@@ -6,12 +6,16 @@ import empRouter from "./routes/empRouter.js";
 let port = 8080;
 let host = "127.0.0.1";
 let app = express();
+
+//Morgan- HTTP Logger Middleware
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/", (req, resp) => {
-  return resp.status(200).json({ msg: "hiii" });
+  return resp.status(200).json({ msg: "application Root Request" });
 });
 
+//forward all employee api to empRouter file
 app.use("/emp", empRouter);
 
 app.listen(port, host, (error) => {
