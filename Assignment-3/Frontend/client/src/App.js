309367fddefc5e2f76221f41e96893857323193e
Naveenkumar506 <naveennani0603@gmail.com>
2025-11-15 00:03:23 +0530

    api.js

--- Assignment-3/Frontend/client/src/App.js
+++ Assignment-3/Frontend/client/src/App.js
@@ -1,1 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import VerifyOTP from "./components/pages/VerifyOTP";
+import "./App.css";
 
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Register</Link> | <Link to="/login">Login</Link> |{" "}
+        <Link to="/verify">Verify OTP</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/verify" element={<VerifyOTP />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
